72e96c18f8ffc0d9484584ba0a56bfacaafb02b3
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-06-01 18:11:10 +0700

    Create getDapAnLan2.js

---
+++ shortened/getDapAnLan2.js
@@ -0,0 +1,25 @@
+//get dap an lan2
+ans = JSON.parse(localStorage.getItem("ans"));
+dapAn = document.getElementsByClassName("text-success");
+for(i=2;i<dapAn.length;i+=3){
+    	var cauHoi =  dapAn[i-1].parentNode.nextElementSibling.innerText.replace(/(\n| |\$|\t)/gm, "");
+	var answer =  dapAn[i].parentNode.nextElementSibling.innerText.replace(/(\n| |\$|\t)/gm, "");
+    if(ans.includes(cauHoi)){
+        continue;
+    }else{    
+    	ans.push(cauHoi);
+	ans.push(answer); 
+    }
+}
+localStorage.setItem("ans",JSON.stringify(ans));
+
+wrongAns2 = [];
+dapAnSai = document.getElementsByClassName("text-danger");
+
+for(i=3;i<dapAnSai.length;i+=3){
+        var cauHoi =  dapAnSai[i-1].parentNode.nextElementSibling.innerText.replace(/(\n| |\$|\t)/gm, "");
+	var answer =  dapAnSai[i].parentNode.nextElementSibling.innerText.replace(/(\n| |\$|\t)/gm, "");
+        wrongAns2.push(cauHoi);
+	wrongAns2.push(answer); 
+}
+localStorage.setItem("wrongAns2",JSON.stringify(wrongAns2));
