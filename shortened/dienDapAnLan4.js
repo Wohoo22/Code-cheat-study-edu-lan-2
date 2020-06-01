37fd69bbd31e40aec835a0d13969632e4b054b02
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-06-01 18:14:20 +0700

    Create dienDapAnLan4.js

---
+++ shortened/dienDapAnLan4.js
@@ -0,0 +1,87 @@
+//dien dap an lan 4
+cauHoi = document.getElementsByClassName("col-11 question-box-title");
+ans = JSON.parse(localStorage.getItem("ans"));
+var buttons = document.querySelectorAll('input[type="radio"]');
+for(i=0;i<cauHoi.length;i++){
+	for(i2=0; i2<ans.length; i2++){
+		if(cauHoi[i].innerText.replace(/(\n| |\$|\t)/gm, "") == ans[i2]){
+            		
+			for(i3=i*4; i3<i*4+4; i3++){
+				if(buttons[i3].parentNode.parentNode.parentNode.lastElementChild.innerText.replace(/(\n| |\$|\t)/gm, "") == ans[i2+1]){
+					buttons[i3].click();
+					buttons[i3].parentNode.parentNode.parentNode.lastElementChild.style.color="red";
+					cauHoi[i].id = "right";
+					for(i3=i*4; i3<i*4+4; i3++){
+						buttons[i3].className  = "right";
+					}
+				}
+			}
+		}
+	}
+}
+wrongAns = JSON.parse(localStorage.getItem("wrongAns"));
+for(i=0;i<cauHoi.length;i++){
+    if(cauHoi[i].id == "right"){
+    continue;
+    }else{
+        for(i2=0;i2<wrongAns.length;i2++){
+            if(cauHoi[i].innerText.replace(/(\n| |\$|\t)/gm, "") == wrongAns[i2]){
+                for(i3=i*4;i3<i*4+4;i3++){
+                	if(buttons[i3].parentNode.parentNode.parentNode.lastElementChild.innerText.replace(/(\n| |\$|\t)/gm, "") == wrongAns[i2+1]){
+                        	buttons[i3].className  = "wrong";
+                    	}
+                }
+            }
+         }
+     }
+}
+wrongAns2 = JSON.parse(localStorage.getItem("wrongAns2"));
+for(i=0;i<cauHoi.length;i++){
+    if(cauHoi[i].id == "right"){
+    continue;
+    }else{
+        for(i2=0;i2<wrongAns2.length;i2++){
+            if(cauHoi[i].innerText.replace(/(\n| |\$|\t)/gm, "") == wrongAns2[i2]){
+                for(i3=i*4;i3<i*4+4;i3++){
+                	if(buttons[i3].parentNode.parentNode.parentNode.lastElementChild.innerText.replace(/(\n| |\$|\t)/gm, "") == wrongAns2[i2+1]){
+                        	buttons[i3].className  = "wrong";
+                    	}
+                }
+            }
+         }
+     }
+}
+wrongAns3 = JSON.parse(localStorage.getItem("wrongAns3"));
+for(i=0;i<cauHoi.length;i++){
+    if(cauHoi[i].id == "right"){
+    continue;
+    }else{
+        for(i2=0;i2<wrongAns3.length;i2++){
+            if(cauHoi[i].innerText.replace(/(\n| |\$|\t)/gm, "") == wrongAns3[i2]){
+                for(i3=i*4;i3<i*4+4;i3++){
+                	if(buttons[i3].parentNode.parentNode.parentNode.lastElementChild.innerText.replace(/(\n| |\$|\t)/gm, "") == wrongAns3[i2+1]){
+                        	buttons[i3].className  = "wrong";
+                    	}
+                }
+            }
+         }
+     }
+}
+for(i=0;i<buttons.length;i++){
+    if(buttons[i].className == "" ){
+        buttons[i].click();
+    }
+}
+$.ajax({
+         type: "POST",
+         url: "/home/saveusertestresult",
+         cache: "false",
+         data: {
+                ExamId: parseInt(prompt("Nhập số")),
+                AnswerPerQuestions: ConvertLocalStoreDataToServerType(testResultLocalObject),
+                SubmitType: 2,
+                },
+         success: function (data) {         
+                 window.location.href = url + data.Result;
+		}
+	})
