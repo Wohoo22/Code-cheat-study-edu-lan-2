91e734adcd367938f2b385fe256b20685961e4e5
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-06-01 21:50:30 +0700

    Update shortCheatCode.js

--- shortCheatCode.js
+++ shortCheatCode.js
@@ -5,12 +5,67 @@ for(i=0;i<buttons.length;i++){
 }
 
 
-//code lấy đáp án đúng và sai lần 1
+//code lấy đáp án đúng và sai (lần làm bài 1)
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {      
       eval(this.responseText);
     }
   }
-xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Cheat-Study.hanoi.edu/master/getAnsQues.js", true);
+xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/getDapAnLan1.js", true);
+xhttp.send();
+
+
+//code tự nhập đáp án đúng và loại trừ các đáp án sai (lần làm bài 2)
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {      
+      eval(this.responseText);
+    }
+  }
+xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/dienDapAnLan2.js", true);
+xhttp.send();
+
+
+//code lấy đáp án đúng và sai (lần làm bài 2)
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {      
+      eval(this.responseText);
+    }
+  }
+xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/getDapAnLan2.js", true);
+xhttp.send();
+
+
+//code tự nhập đáp án đúng và loại trừ các đáp án sai (lần làm bài 3)
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {      
+      eval(this.responseText);
+    }
+  }
+xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/dienDapAnLan3.js", true);
+xhttp.send();
+
+
+//code lấy đáp án đúng và sai (lần làm bài 3)
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {      
+      eval(this.responseText);
+    }
+  }
+xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/getDapAnLan3.js", true);
+xhttp.send();
+
+
+//code tự nhập đáp án đúng và loại trừ các đáp án sai (lần làm bài 4)
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {      
+      eval(this.responseText);
+    }
+  }
+xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/dienDapAnLan4.js", true);
 xhttp.send();
