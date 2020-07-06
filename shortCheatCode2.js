4241428a899721a03d4a090f082efc5b7a407d04
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-07-06 22:45:35 +0700

    Update shortCheatCode2.js

--- shortCheatCode2.js
+++ shortCheatCode2.js
@@ -1,116 +1,79 @@
-//pick and get 1
-function copyStringToClipboard(str) {var el = document.createElement('textarea');el.value = str;el.setAttribute('readonly', '');el.style = {position: 'absolute', left: '-9999px'};document.body.appendChild(el);el.select();document.execCommand('copy');document.body.removeChild(el);}
-var buttons = document.querySelectorAll('input[type="radio"]');
-for(i=0;i<buttons.length;i++){
-    buttons[i].click();
-}
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {      
-      copyStringToClipboard(this.responseText);
+var m = parseInt(prompt("Luot"));
+
+function a() {
+    if (m == 1.1) {
+        //-1.1-code tự chọn cùng đáp án lần đầu (optional)
+        var buttons = document.querySelectorAll('input[type="radio"]');
+        for (i = 0; i < buttons.length; i++) {
+            buttons[i].click();
+        }
+
     }
-  }
-xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/getDapAnLan1.js", true);
-xhttp.send();
+    if (m == 1.2) {
+        //-1.2-code lấy đáp án đúng và sai (lần làm bài 1)
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                eval(this.responseText);
+            }
+        }
+        xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/getDapAnLan1.js", true);
+        xhttp.send();
 
-//pick and get 2
-function copyStringToClipboard(str) {var el = document.createElement('textarea');el.value = str;el.setAttribute('readonly', '');el.style = {position: 'absolute', left: '-9999px'};document.body.appendChild(el);el.select();document.execCommand('copy');document.body.removeChild(el);}
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {      
-      eval(this.responseText);
     }
-  }
-xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/dienDapAnLan2.js", true);
-xhttp.send();
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {      
-      copyStringToClipboard(this.responseText);
+    if (m == 2.1) {
+        //-2.1-code tự nhập đáp án đúng và loại trừ các đáp án sai (lần làm bài 2)
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                eval(this.responseText);
+            }
+        }
+        xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/dienDapAnLan2.js", true);
+        xhttp.send();
     }
-  }
-xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/getDapAnLan2.js", true);
-xhttp.send();
-
-//pick and get 3
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {      
-      eval(this.responseText);
+    if (m == 2.2) {
+        //-2.2-code lấy đáp án đúng và sai (lần làm bài 2)
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                eval(this.responseText);
+            }
+        }
+        xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/getDapAnLan2.js", true);
+        xhttp.send();
     }
-  }
-xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/dienDapAnLan3.js", true);
-xhttp.send();
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {      
-      copyStringToClipboard(this.responseText);
+    if (m == 3.1) {
+        //-3.1-code tự nhập đáp án đúng và loại trừ các đáp án sai (lần làm bài 3)
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                eval(this.responseText);
+            }
+        }
+        xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/dienDapAnLan3.js", true);
+        xhttp.send();
     }
-  }
-xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/getDapAnLan3.js", true);
-xhttp.send();
-
-//pick 4
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {      
-      eval(this.responseText);
+    if (m == 3.2) {
+        //-3.2-code lấy đáp án đúng và sai (lần làm bài 3)
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                eval(this.responseText);
+            }
+        }
+        xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/getDapAnLan3.js", true);
+        xhttp.send();
     }
-  }
-xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/dienDapAnLan4.js", true);
-xhttp.send();
-
-
-
-
-//case 1: 10 diem (lay json dap an o phan chi tiet bai lam)
-ans =[];
-dapAn = document.getElementsByClassName("text-success");
-for(i=2;i<dapAn.length;i+=3){
-    	var cauHoi =  dapAn[i-1].parentNode.nextElementSibling.innerText.replace(/\s/g, "");
-	var answer =  dapAn[i].parentNode.nextElementSibling.innerText.replace(/\s/g, "");
-    	ans.push(cauHoi);
-	ans.push(answer); 
-}
-localStorage.setItem("ans",JSON.stringify(ans));
-
-
-
-
-//case 2: <10 --> get dap an o phan lam bai sau khi lam dung:
-//pickRightAnswer
-cauHoi = document.getElementsByClassName("col-md-11 col-10 question-box-title");
-ans = JSON.parse(localStorage.getItem("ans"));
-var buttons = document.querySelectorAll('input[type="radio"]');
-pickCount=0;
-for(i=0;i<cauHoi.length;i++){
-	for(i2=0; i2<ans.length; i2++){
-		if(cauHoi[i].innerText.replace(/\s/g, "") == ans[i2]){
-			for(i3=i*4; i3<i*4+4; i3++){
-				if(buttons[i3].parentNode.parentNode.parentNode.lastElementChild.innerText.replace(/\s/g, "") == ans[i2+1]){
-					buttons[i3].click();
-					buttons[i3].parentNode.parentNode.parentNode.lastElementChild.style.color="red";
-					pickCount++;
-				}
-			}
-		}
-	}
-}
-alert("Ấn OK để chọn "+pickCount+"/"+cauHoi.length+" câu ("
-      +10/cauHoi.length+" điểm 1 câu), các câu đúng sẽ chuyển thành màu đỏ, bạn có thể chọn lại để điểm thấp hơn.");
-//getAnswerInTest
-cauHoi = document.getElementsByClassName("col-md-11 col-10 question-box-title");
-var dapAn = [];
-var buttons = document.querySelectorAll('input[type="radio"]');
-for(i=0; i<buttons.length; i++){
-    if(buttons[i].checked){
-        dapAn.push(buttons[i].parentNode.parentNode.parentNode.lastElementChild.innerText.replace(/\s/g, ""));
+    if (m == 4) {
+        //-4-code tự nhập đáp án đúng và loại trừ các đáp án sai (lần làm bài 4)
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                eval(this.responseText);
+            }
+        }
+        xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/dienDapAnLan4.js", true);
+        xhttp.send();
     }
 }
-var lastAns = [];
-for(i=0;i<cauHoi.length;i++){
-	lastAns .push(cauHoi[i].innerText.replace(/\s/g, ""));
-	lastAns .push(dapAn[i]);
-}
-localStorage.setItem("lastAnswerPicked",JSON.stringify(lastAns));
-
