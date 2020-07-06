4700f762743b00ccb6aee49201d95b09515df107
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-07-06 19:03:23 +0700

    Update shortCheatCode.js

--- shortCheatCode.js
+++ shortCheatCode.js
@@ -95,3 +95,36 @@ for(i=0; i<buttons.length; i++){
         buttons[i].parentNode.parentNode.parentNode.lastElementChild.style.color="red";
     }
 }
+
+//get ans
+ans =[];
+dapAn = document.getElementsByClassName("text-success");
+for(i=2;i<dapAn.length;i+=3){
+    	var cauHoi =  dapAn[i-1].parentNode.nextElementSibling.innerText.replace(/\s/g, "");
+	var answer =  dapAn[i].parentNode.nextElementSibling.innerText.replace(/\s/g, "");
+    	ans.push(cauHoi);
+	ans.push(answer); 
+}
+localStorage.setItem("ans",JSON.stringify(ans));
+
+
+//pick right ans
+cauHoi = document.getElementsByClassName("col-md-11 col-10 question-box-title");
+ans = JSON.parse(localStorage.getItem("ans"));
+var buttons = document.querySelectorAll('input[type="radio"]');
+pickCount=0;
+for(i=0;i<cauHoi.length;i++){
+	for(i2=0; i2<ans.length; i2++){
+		if(cauHoi[i].innerText.replace(/\s/g, "") == ans[i2]){
+			for(i3=i*4; i3<i*4+4; i3++){
+				if(buttons[i3].parentNode.parentNode.parentNode.lastElementChild.innerText.replace(/\s/g, "") == ans[i2+1]){
+					buttons[i3].click();
+					buttons[i3].parentNode.parentNode.parentNode.lastElementChild.style.color="red";
+					pickCount++;
+				}
+			}
+		}
+	}
+}
+alert("Ấn OK để chọn "+pickCount+"/"+cauHoi.length+" câu ("
+      +10/cauHoi.length+" điểm 1 câu), các câu đúng sẽ chuyển thành màu đỏ, bạn có thể chọn lại để điểm thấp hơn.");
