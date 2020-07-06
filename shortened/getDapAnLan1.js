933af146048988ce904e8bb202faf5d3473422bc
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-07-06 18:48:32 +0700

    Update getDapAnLan1.js

--- shortened/getDapAnLan1.js
+++ shortened/getDapAnLan1.js
@@ -17,13 +17,4 @@ for(i=3;i<dapAnSai.length;i+=3){
 	wrongAns.push(answer); 
 }
 localStorage.setItem("wrongAns",JSON.stringify(wrongAns));
-//code tự nhập đáp án đúng và loại trừ các đáp án sai (lần làm bài 2)
-function copyStringToClipboard (str) {var el = document.createElement('textarea');el.value = str;el.setAttribute('readonly', '');el.style = {position: 'absolute', left: '-9999px'};document.body.appendChild(el);el.select();document.execCommand('copy');document.body.removeChild(el);}
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {      
-      copyStringToClipboard(this.responseText);
-    }
-  }
-xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/dienDapAnLan2.js", true);
-xhttp.send();
+
