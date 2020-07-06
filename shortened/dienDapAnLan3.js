2ac03a09c41d554234c7f08752a18cd6a683bff1
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-07-06 18:47:33 +0700

    Update dienDapAnLan3.js

--- shortened/dienDapAnLan3.js
+++ shortened/dienDapAnLan3.js
@@ -66,16 +66,7 @@ $.ajax({
                 SubmitType: 2,
                 },
          success: function (data) {         
-               location.reload();
-		 function copyStringToClipboard (str) {var el = document.createElement('textarea');el.value = str;el.setAttribute('readonly', '');el.style = {position: 'absolute', left: '-9999px'};document.body.appendChild(el);el.select();document.execCommand('copy');document.body.removeChild(el);}
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {      
-      copyStringToClipboard(this.responseText);
-    }
-  }
-xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/getDapAnLan3.js", true);
-xhttp.send();
+               
 		}
 	})
 
