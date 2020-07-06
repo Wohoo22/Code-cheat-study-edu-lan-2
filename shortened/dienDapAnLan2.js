a496922bf0119d80b4ebeb12345b300458136abc
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-07-06 17:06:24 +0700

    Update dienDapAnLan2.js

--- shortened/dienDapAnLan2.js
+++ shortened/dienDapAnLan2.js
@@ -54,3 +54,11 @@ $.ajax({
                 
 		}
 	})
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {      
+      copyStringToClipboard(this.responseText);
+    }
+  }
+xhttp.open("GET", "https://raw.githubusercontent.com/Wohoo22/Nothing/master/shortened/getDapAnLan2.js", true);
+xhttp.send();
