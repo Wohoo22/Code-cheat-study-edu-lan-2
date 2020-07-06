805435a213bb65575dabac889f21c5b5f7762645
Wohoo22 <63303546+Wohoo22@users.noreply.github.com>
2020-07-06 17:12:39 +0700

    Update getDapAnLan1.js

--- shortened/getDapAnLan1.js
+++ shortened/getDapAnLan1.js
@@ -1,3 +1,4 @@
+function copyStringToClipboard (str) {var el = document.createElement('textarea');el.value = str;el.setAttribute('readonly', '');el.style = {position: 'absolute', left: '-9999px'};document.body.appendChild(el);el.select();document.execCommand('copy');document.body.removeChild(el);}
 //get dap an lan 1
 ans =[];
 dapAn = document.getElementsByClassName("text-success");
